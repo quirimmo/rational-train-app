139cb52eacdd8e7913b9bbdd654e2d0f60b5ce70
quirimmo <quirimmo@gmail.com>
2017-09-03 20:43:28 +0100

    implemented distance and check if you are in time

--- src/controllers/train-panel.controller.js
+++ src/controllers/train-panel.controller.js
@@ -8,14 +8,22 @@
     function TrainPanelController($interval, $mdDialog, train, trainPosition, currentPosition, startingStation, trainService, NavigatorGeolocation, NgMap, distanceService) {
         var vm = this;
 
+        const UPDATE_TRAIN_POSITION_TIMEOUT = 30 * 1000;
+        
+        vm.train = train;
+        vm.trainPosition = trainPosition;
+        vm.currentPosition = currentPosition;
+        vm.distanceToTheStation;
+        vm.timeToTheStation;
+        vm.timestampArrivingToTheStation; 
+        vm.timestampTrainDepartureFromTheStation;
+        let updateTrainPositionInterval = undefined;
+
         vm.closePanel = closePanel;
         vm.getTrainPosition = getTrainPosition;
         vm.getCurrentPosition = getCurrentPosition;
         vm.getStationPosition = getStationPosition;
-        vm.train = train;
-        vm.trainPosition = trainPosition;
-        vm.currentPosition = currentPosition;
-        let updateTrainPositionInterval = undefined;
+        vm.isLate = isLate;
 
         init();
 
@@ -23,37 +31,31 @@
             console.log(vm.train);
             console.log(vm.trainPosition);
             if (angular.isDefined(vm.trainPosition) && vm.trainPosition.TrainStatus === 'R') {
-                updateTrainPositionInterval = $interval(updateTrainPosition, 30000);
+                // my idea was to create a real time tracking of the train, but it looks like irishtrail doesn't update the values of latitude and longitude constantly but just after a while 
+                updateTrainPositionInterval = $interval(updateTrainPosition, UPDATE_TRAIN_POSITION_TIMEOUT);
             }
             distanceService.calculateDistance(vm.currentPosition.latitude, vm.currentPosition.longitude, startingStation.StationLatitude, startingStation.StationLongitude)
             .then(data => {
-                console.log(data);
+                vm.distanceToTheStation = data[0].distance.text;
+                vm.timeToTheStation = data[0].duration.text;
+
+                let expDepart = vm.train.Expdepart;
+                let d = new Date();
+                let parts = expDepart.match(/(\d+):(\d+)/);
+                let hours = parts[1];
+                let minutes = parts[2];
+                d.setHours(hours);
+                d.setMinutes(minutes);
+                
+                let d2 = new Date();
+                d2.setMinutes(data[0].duration.value / 60);
+
+                vm.timestampTrainDepartureFromTheStation = d.getTime(); 
+                vm.timestampArrivingToTheStation = d2.getTime();
             })
             .catch(err => {
                 throw new Error(err);
             });
-            // var origin1 = new google.maps.LatLng(+vm.currentPosition.latitude, +vm.currentPosition.longitude);
-            // var destination1 = new google.maps.LatLng(+vm.trainPosition.TrainLatitude, +vm.trainPosition.TrainLongitude);
-    
-            // var service = new google.maps.DistanceMatrixService();
-            // service.getDistanceMatrix({
-            //     origins: [origin1],
-            //     destinations: [destination1],
-            //     travelMode: 'WALKING'
-            // }, callback);
-    
-            // function callback(response, status) {
-            //     console.log(response);
-            //     if (status === 'OK') {
-            //         response.rows.forEach(row => {
-            //             row.elements.forEach(element => {
-            //                 console.log(element.distance);
-            //                 console.log(element.duration);
-            //             });
-            //         });
-            //     }
-            // }
-
         }
 
         function closePanel() {
@@ -77,6 +79,10 @@
                         throw new Error(err);
                     });
             });
+        }
+
+        function isLate() {
+            return (+vm.timestampTrainDepartureFromTheStation - +vm.timestampArrivingToTheStation) < 0;
         }
 
         function getTrainPosition() {
