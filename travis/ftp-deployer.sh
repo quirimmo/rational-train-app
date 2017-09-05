c9766df457b50621576a5697bfcf05c2ba117a49
quirimmo <quirimmo@gmail.com>
2017-09-05 20:48:54 +0100

    using right script for upload

--- travis/ftp-deployer.sh
+++ travis/ftp-deployer.sh
@@ -5,12 +5,12 @@ set -ev
 
 ftp_transfer () {
 	cd dist
-	find . -type f -exec curl --retry 3 --ftp-create-dirs  -u $1:$2 -T {} ftp://$3/{}  \;
+	find . -type f -exec curl --retry 3 --ftp-create-dirs  -u $2:$3 -T {} ftp://$2/{}  \;
 }
 
 # if destination branch of the PR is master
 # then ftp app to the server
 if [ $(is_master_PR) = true ]; then
-	ftp_prod_address=$BITWEED_FTP_DOMAIN
-	ftp_transfer $FTP_USER $FTP_PASSWORD $ftp_prod_address
+	ftp_dev_address=$BITWEED_FTP_DOMAIN/$TRAVIS_PULL_REQUEST
+	ftp_transfer $FTP_USER $FTP_PASSWORD $ftp_dev_address
 fi
