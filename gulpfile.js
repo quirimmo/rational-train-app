9fd0b0e3755ac6798faee0fa6934ae27a4a6ed70
quirimmo <quirimmo@gmail.com>
2017-09-05 23:05:20 +0100

    trying to remove ftp upload error for node components file

--- gulpfile.js
+++ gulpfile.js
@@ -26,14 +26,14 @@
 
     const PATHS = {
         NODE_MODULES_COMPONENTS: [
-            './node_modules/jquery/dist/jquery.js',
-            './node_modules/angular/angular.js',
-            './node_modules/angular-messages/angular-messages.js',
-            './node_modules/angular-aria/angular-aria.js',
-            './node_modules/angular-animate/angular-animate.js',
-            './node_modules/angular-material/angular-material.js',
-            './node_modules/angular-ui-router/release/angular-ui-router.js',
-            './node_modules/ngmap/build/scripts/ng-map.js'
+            './node_modules/jquery/dist/jquery.min.js',
+            './node_modules/angular/angular.min.js',
+            './node_modules/angular-messages/angular-messages.min.js',
+            './node_modules/angular-aria/angular-aria.min.js',
+            './node_modules/angular-animate/angular-animate.min.js',
+            './node_modules/angular-material/angular-material.min.js',
+            './node_modules/angular-ui-router/release/angular-ui-router.min.js',
+            './node_modules/ngmap/build/scripts/ng-map.min.js'
         ],
         MAIN_INDEX: './src/index.html',
         SOURCE_FILES: './src/**/*.*',
